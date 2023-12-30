3f1cddc6193095454d7e62d433711ea03c407f1f
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2023-12-30 15:31:44 +0530

    nav+ header+ herosection

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-import classes from './App.module.scss'
+import styled,{ ThemeProvider } from 'styled-components';
+import './App.css';
+import { darkTheme } from './utils/Themes';
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import Skills from './components/Skills';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const Body = styled.div`
+  background-color:${({theme}) => theme.bg};
+  width:100%; 
+  height:100%;
+  overflow-x:hidden;
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`
 const App = ()=>{
   return (
-    <div>
-      <h1 className={`${classes['app']}`}>Hello</h1>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+
+      <Navbar></Navbar>
+      <Body>
+        <HeroSection></HeroSection>
+        <Wrapper>
+        <Skills></Skills>
+
+        </Wrapper>
+      </Body>
+      </Router>
+    </ThemeProvider>
   )
 }
 
